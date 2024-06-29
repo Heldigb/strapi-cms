f1db2b782f20b7e8e46af319a22d2efbfa0fa7f0
Nikita Ananjevs <ananjevs.n@nakukop.com>
2024-06-30 00:31:39 +0200

    feat: starter

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = () => ({    "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: "<netlify-access-token>",
+      sites: [
+        {
+          name: 'Site 1',
+          id: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+          buildHook: "https://api.netlify.com/build_hooks/<hook_id>",
+          branch: 'master' // optional
+        }
+      ]
+    },
+  }
+});
